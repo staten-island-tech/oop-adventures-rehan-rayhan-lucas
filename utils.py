097ddb1d0c94b0622834b1c6c30b0039f87ef927
LucasLin69 <lucasl92@nycstudents.net>
2024-12-19 10:17:14 -0500

    wdadwadwadwafesfes

--- utils.py
+++ utils.py
@@ -4,24 +4,21 @@ class utility:
         self.name = name
         self.description = description
         self.cost = cost
-
     def buy(self):
         print(f"Buying {self.name}: {self.description} for {self.cost}!")
-
     def writesmth(self):
         return f"{self.name}: {self.description} (Cost: {self.cost})"
-
-glassermagnifyer = utility("Magnifying Glass", "idk", 1)
-notebooker = utility("Notebook", "idk", 1)
-lockerpicker = utility("Lockpick", "idk", 1)
-lighterflasher = utility("Flashlight", "idk", 1)
-cameraer = utility("Camera", "idk", 1)
-kitterdisguiser = utility("Disguising Kit", "idk", 1)
-mapper = utility("Map", "idk", 1)
-keyer = utility("Key", "idk", 1)
-packerbacker = utility("Backpack", "idk", 1)
-kitterfingerprinter = utility("Fingerprinting Kit", "idk", 1)
-
+#Creating Utilities
+glassermagnifyer = utility("Magnifying Glass", "To examine/find evidence. 25 percent chance of breaking", 5)
+notebooker = utility("Notebook", "To write down notes.", 5)
+lockerpicker = utility("Lockpick", "To open locked doors or compartments. Has a 25 percent of breaking.", 3)
+lighterflasher = utility("Flashlight", "You light up my world! A light source.", 20)
+cameraer = utility("Camera", "To keep memories! One time usage. Photos can be put into notebook.", 3)
+kitterdisguiser = utility("Disguising Kit", "For camouflage purposes.", 15)
+mapper = utility("Map", "For recording the surroundings.", 1)
+keyer = utility("Key", "To unlock locked doors (duh).", 10)
+packerbacker = utility("Backpack", "To increase your inventory space.", 25)
+kitterfingerprinter = utility("Fingerprinting Kit", "To identify fingerprints.", 15)
 utilities = [
     glassermagnifyer,
     notebooker,
